270fde073c4a7a079067e7a9816d1ac6e143f6e9
hiroaki.kubota <hiroaki.kubota@mail.crumb.jp>
2013-08-23 15:12:34 +0900

    Temporary commit

--- clusterize/jobs/kmeans.js
+++ clusterize/jobs/kmeans.js
@@ -1,69 +1,6 @@
 function main(jobctl,options) {
-	
 	print('== FIRST ==');
-	var ret = jobctl.put( function(){
-		return new Job({
-			empty_dst : function(){
-				var psrc = utils.parseCollection(this.SRCCOL);
-				return psrc.db + '.kmeans.'   +psrc.col;
-			},
-			prepare_create : function() {
-				var pdst = utils.parseCollection(this.DSTCOL);
-				utils.cleanCollections(this.DSTCOL+'\.');
-				return {ok:1};
-			},
-			create_job : function() {
-				return {ok:1};
-			},
-
-			prepare_run : function(){
-				this.initial_cluster = this.ARGS['I'];
-				this.cluster_field   = this.ARGS['C'];
-				this.field           = this.ARGS['F'];
-
-				this.meta =	utils.getmeta(this.src);
-				this.meta.vector = this.SRCCOL;
-				this.meta.kmeans = {
-					data    : this.DSTCOL + '.it1.data',
-					cluster : this.DSTCOL + '.it1.cluster',
-					field   : this.ARGS['F']
-				};
-				this.cluster = utils.getWritableCollection(this.meta.kmeans.cluster);
-				this.data    = utils.getWritableCollection(this.meta.kmeans.data);
-
-				return {ok:1};
-			},
-			run : function() {
-				var cs = utils.getClusters(this.initial_cluster,this.cluster_field);
-				for ( var c in cs ) {
-					this.cluster.save({ 
-						_id : c ,
-						s : 0, 
-						loc : cs[c] , 
-						st: 0 });
-				}
-		
-				var _c_src = this.src.find(utils.IGNORE_META);
-				while(_c_src.hasNext()){
-					var data = _c_src.next();
-					var loc  = utils.getField(data,this.field);
-					if ( loc ) {
-						this.data.save({
-							_id:data._id,
-							loc:loc,
-							st: 0
-						});					
-					}
-				}
-
-				return {
-					ok : 1,
-					msg : 'success',
-					data : { meta: this.meta }
-				}
-			}
-		});
-	},options);
+	var ret = first_job();
 
 	var NS = ret.dst;
 	options.args['meta'] = ret.data.meta;
@@ -79,9 +16,105 @@ function main(jobctl,options) {
 		options.dst = CUR_D;
 		options.args['prev_cluster'] = options.args['cluster'];
 		options.args['cluster'] = PREV_C;
-		
+
+    ret = deploy_data();
+
+		print(' DIFF : ' + ret.data.cdiff);
+		if ( ret.ok === 2 ) {
+			utils.getWritableCollection(options.dst).drop();
+			break;
+		}
+		print('== '+i+' CLUSTER ==');
+		// cluster iterate
+		options.src = PREV_C;
+		options.dst = CUR_C;
+		options.args['data'] = CUR_D;
+
+    ret = move_center();
+	}
+
+	var meta = options.args['meta'];
+	meta.kmeans.data   = NS+'.fin.data';
+	meta.kmeans.cluster= NS+'.fin.cluster';
+
+	var pns = utils.parseCollection(NS);
+	
+	utils.getWritableCollection(options.args['data']).renameCollection(pns.col+'.fin.data');
+	utils.getWritableCollection(options.args['cluster']).renameCollection(pns.col+'.fin.cluster');
+
+	utils.setmeta(utils.getWritableCollection(meta.kmeans.data),meta);
+	utils.setmeta(utils.getWritableCollection(meta.kmeans.cluster),meta);
+  return;
+
+  function first_job() {
+	  return ret = jobctl.put( function(){
+		  return new Job({
+			  empty_dst : function(){
+				  var psrc = utils.parseCollection(this.SRCCOL);
+				  return psrc.db + '.kmeans.'   +psrc.col;
+			  },
+			  prepare_create : function() {
+				  var pdst = utils.parseCollection(this.DSTCOL);
+				  utils.cleanCollections(this.DSTCOL+'\.');
+				  return {ok:1};
+			  },
+			  create_job : function() {
+				  return {ok:1};
+			  },
+        
+			  prepare_run : function(){
+				  this.initial_cluster = this.ARGS['I'];
+				  this.cluster_field   = this.ARGS['C'];
+				  this.field           = this.ARGS['F'];
+          
+				  this.meta =	utils.getmeta(this.src);
+				  this.meta.vector = this.SRCCOL;
+				  this.meta.kmeans = {
+					  data    : this.DSTCOL + '.it1.data',
+					  cluster : this.DSTCOL + '.it1.cluster',
+					  field   : this.ARGS['F']
+				  };
+				  this.cluster = utils.getWritableCollection(this.meta.kmeans.cluster);
+				  this.data    = utils.getWritableCollection(this.meta.kmeans.data);
+          
+				  return {ok:1};
+			  },
+			  run : function() {
+				  var cs = utils.getClusters(this.initial_cluster,this.cluster_field);
+				  for ( var c in cs ) {
+					  this.cluster.save({ 
+						    _id : c ,
+						  s : 0, 
+						  loc : cs[c] , 
+						  st: 0 });
+				  }
+		      
+				  var _c_src = this.src.find(utils.IGNORE_META);
+				  while(_c_src.hasNext()){
+					  var data = _c_src.next();
+					  var loc  = utils.getField(data,this.field);
+					  if ( loc ) {
+						  this.data.save({
+							    _id:data._id,
+							  loc:loc,
+							  st: 0
+						  });					
+					  }
+				  }
+          
+				  return {
+					  ok : 1,
+					  msg : 'success',
+					  data : { meta: this.meta }
+				  }
+			  }
+		  });
+	  },options);
+  }
+
+  function deploy_data(){
 		// data iterate
-		var ret = jobctl.put( function(){
+		return jobctl.put( function(){
 			return new Map({
 				prepare_create : function(){
 					this.dst.ensureIndex({'c':1});
@@ -100,7 +133,7 @@ function main(jobctl,options) {
 					if ( this.ARGS['prev_cluster'] ){
 						var cs_history = utils.getClusters(this.ARGS['prev_cluster']);
 						this.cdiff = 0;
-
+            
 						for ( var c in this.cs ){
 							this.cdiff += utils.diffVector(this.cs[c].loc,cs_history[c].loc);
 						}	
@@ -154,18 +187,10 @@ function main(jobctl,options) {
 				}
 			},true);
 		},options);
-		print(' DIFF : ' + ret.data.cdiff);
-		if ( ret.ok === 2 ) {
-			utils.getWritableCollection(options.dst).drop();
-			break;
-		}
-		print('== '+i+' CLUSTER ==');
-		// cluster iterate
-		options.src = PREV_C;
-		options.dst = CUR_C;
-		options.args['data'] = CUR_D;
+  }
 
-		var ret = jobctl.put( function(){
+  function move_center(){
+		return jobctl.put( function(){
 			return new Map({
 				prepare_create : function(){
 					return { ok:1 };
@@ -197,60 +222,5 @@ function main(jobctl,options) {
 				}
 			},true);
 		},options);
-	}
-	var meta = options.args['meta'];
-	meta.kmeans.data   = NS+'.fin.data';
-	meta.kmeans.cluster= NS+'.fin.cluster';
-
-	var pns = utils.parseCollection(NS);
-	
-print(options.args['data'] + ' => ' + meta.kmeans.data);
-print(options.args['cluster'] + ' => ' + meta.kmeans.cluster);
-
-	utils.getWritableCollection(options.args['data']).renameCollection(pns.col+'.fin.data');
-	utils.getWritableCollection(options.args['cluster']).renameCollection(pns.col+'.fin.cluster');
-
-	utils.setmeta(utils.getWritableCollection(meta.kmeans.data),meta);
-	utils.setmeta(utils.getWritableCollection(meta.kmeans.cluster),meta);
-	
+  }
 }
-
-
-
-function Kmeans(src) {
-	this.SRC = src;
-	var src_split = src.split('\.');
-	this.DB = src_split.shift();
-//	this.db = db.getMongo().getDB(this.DB);
-//	this.pdb= _pmongo.getDB(this.DB);
-	this.NS = src_split.join('\.');
-	this.FIN_C  = 'kmeans.'+this.NS+'.fin.cluster';
-	this.FIN_D  = 'kmeans.'+this.NS+'.fin.data';
-	this.cs_history = null;
-	this.N = 0;
-	this._src = utils.getCollection(this.SRC);
-	this.meta=	utils.getmeta(this._src);
-}
-
-
-
-Kmeans.prototype.finish = function(){
-	var prev_c = this.PREV_C;
-	var prev_d = this.PREV_D;
-
-	var JOBID = 2;
-	if ( utils.sync_job(this._job,JOBID) ) {
-		utils.getWritableCollection(prev_c).renameCollection(this.FIN_C);
-		utils.getWritableCollection(prev_d).renameCollection(this.FIN_D);
-		
-		this.meta.vector = this.SRC;
-		this.meta.data   = this.DB + '.' + this.FIN_D;
-		this.meta.cluster= this.DB + '.' + this.FIN_C;
-		delete(this.meta._id);
-		utils.setmeta(utils.getWritableCollection(this.meta.cluster),this.meta);
-		utils.setmeta(utils.getWritableCollection(this.meta.data),this.meta);
-		printjson(this.meta);
-		utils.end_job(this._job,JOBID);
-	}
-}
-
